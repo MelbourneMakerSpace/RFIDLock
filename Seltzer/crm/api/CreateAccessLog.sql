e641ad924e74346a3fa3a91f9154e1c611102271
Josh P <ramgarden@gmail.com>
2025-06-10 03:48:24 +0000

    mysql is case sensitive with table names. all existing code needs table name lowercase "accesslog".

--- Seltzer/crm/api/CreateAccessLog.sql
+++ Seltzer/crm/api/CreateAccessLog.sql
@@ -7,7 +7,7 @@
 -- REASON = door lock returns "key not found" or "balance owed = $$"
 -- This table is written to by the door lock as keys are swiped.
 -- Josh Pritt June 9, 2025
-CREATE TABLE ACCESSLOG (
+CREATE TABLE accesslog (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     SERIAL VARCHAR(20) NOT NULL,
     ACCESS TINYINT(1) NOT NULL, -- MySQL uses TINYINT(1) for boolean-like values
